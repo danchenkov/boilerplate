dce569514cf4e06383ee330be79ab8a6a11eeee2
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-07-01 21:52:12 -0700

    wire up jquery; modernizr; bootstrap

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './scss/main';
+
+$(document).ready(function(){
+  if(Modernizr){
+    console.log("Modernizr is active");
+  }
+
+  var toggleButton = $('<input/>').attr({ type: 'button', id: 'toggleme', name:'toggleme', class: 'btn btn-primary', value:'Toggle!' });
+
+	$("#main>section").append(toggleButton);
+
+	toggleButton.on("click", function() {
+		$("#main>section>.container").toggle();
+	})
+});
