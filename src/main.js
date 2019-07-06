8b4de92b9e96e1290f6243b9faff8f31bb6183a7
Alexei Danchenkov <adanchenkov@quality.local>
2019-07-06 10:53:42 -0700

    Dynamic posts

--- src/main.js
+++ src/main.js
@@ -16,19 +16,19 @@ $(document).ready(function(){
 	Vue.config.devtools = true;
 
 	Vue.component('post', {
-	  props: ['title'],
-	  template: '<h3>{{ title }}</h3>'
+		props: ['title'],
+		template: '<h3>{{ title }}</h3>'
 	});
 
 	new Vue({
-	  el: '#app'
-	  // data: {
-	  //   posts: [
-	  //     { id: 1, title: 'My journey with Vue' },
-	  //     { id: 2, title: 'Blogging with Vue' },
-	  //     { id: 3, title: 'Why Vue is so fun' }
-	  //   ]
-	  // }
+		el: '#app',
+		data: {
+		posts: [
+			{ id: 1, title: 'My journey with Vue' },
+			{ id: 2, title: 'Blogging with Vue' },
+			{ id: 3, title: 'Why Vue is so fun' }
+		]
+		}
 	});
 });
 
