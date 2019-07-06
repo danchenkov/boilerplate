aa9df6101d7d0f48e4cf8aa985c10479107861c2
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-07-06 00:23:09 -0700

    not working

--- src/main.js
+++ src/main.js
@@ -12,4 +12,23 @@ $(document).ready(function(){
 	toggleButton.on("click", function() {
 		$("#main>section>.container").toggle();
 	});
+
+	Vue.config.devtools = true;
+
+	Vue.component('post', {
+	  props: ['title'],
+	  template: '<h3>{{ title }}</h3>'
+	});
+
+	new Vue({
+	  el: '#app'
+	  // data: {
+	  //   posts: [
+	  //     { id: 1, title: 'My journey with Vue' },
+	  //     { id: 2, title: 'Blogging with Vue' },
+	  //     { id: 3, title: 'Why Vue is so fun' }
+	  //   ]
+	  // }
+	});
 });
+
