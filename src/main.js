2e1519fe2bd76eadfbaa59be50964fce3c12f821
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-09-17 20:38:16 -0700

    split main and vue; populate form

--- src/main.js
+++ src/main.js
@@ -13,29 +13,6 @@ $(document).ready(function() {
 		$("#main>section.toggleable").toggle();
 	});
 
-	Vue.config.devtools = true;
-
-	Vue.component('post', {
-		props: ['post'],
-		template: '<h3>{{ post.title }}</h3>'
-	});
-
-	new Vue({
-		el: '#app',
-		data: {
-			posts: [
-				{ id: 1, title: 'Target A1' },
-				{ id: 2, title: 'Target B2' },
-				{ id: 3, title: 'Target C3' },
-				{ id: 4, title: 'Target D4' },
-				{ id: 5, title: 'Target E5' },
-				{ id: 6, title: 'Target F6' },
-				{ id: 7, title: 'Target G7' },
-				{ id: 8, title: 'Target H8' }
-			]
-		}
-	});
-
 	console.log("Hello from JavaScript!");
 	console.log(moment().startOf('day').fromNow());
 });
