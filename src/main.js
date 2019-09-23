2f8d62534e495a8c57b014b6d7c3b09e3adbb8eb
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-09-23 12:23:54 -0700

    updated with more examples from the book

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import './scss/main';
 
+function setCheckedValueOfRadioButtonGroup(name, value) {
+	var radios = document.getElementsByName(name);
+	for (var j = 0; j < radios.length; j++) {
+		if (radios[j].value == value) {
+			radios[j].checked = true;
+			break;
+		}
+	}
+}
+
+function setCheckedValueOfCheckbox(name, value) {
+	var checkbox = document.getElementsByName(name);
+	for (var j = 0; j < checkbox.length; j++) {
+		if (checkbox[j].value == value) {
+			checkbox[j].checked = true;
+		}
+	}
+}
+
+function setValueOfFormField(name, value) {
+	var fields = document.getElementsByName(name);
+	for (var j = 0; j < fields.length; j++) {
+		fields[j].value = value;
+	}
+}
+
 $(document).ready(function() {
 	if (Modernizr) {
 		console.log("Modernizr is active");
@@ -15,4 +41,16 @@ $(document).ready(function() {
 
 	console.log("Hello from JavaScript!");
 	console.log(moment().startOf('day').fromNow());
+
+	if (window.location.pathname == "/sample-form.html") {
+		var urlParams = new URLSearchParams(window.location.search);
+		setCheckedValueOfRadioButtonGroup('title', urlParams.get('title'))
+		setValueOfFormField('first_name', urlParams.get('first_name'))
+		setValueOfFormField('last_name', urlParams.get('last_name'))
+		setValueOfFormField('email', urlParams.get('email'))
+		setValueOfFormField('phone', urlParams.get('phone'))
+		setValueOfFormField('country', urlParams.get('country'))
+		setValueOfFormField('password', urlParams.get('password'))
+		setCheckedValueOfCheckbox('terms', urlParams.get('terms'))
+	}
 });
