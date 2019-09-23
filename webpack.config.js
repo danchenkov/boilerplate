bbe473aa14ef0ed2680a9b7fd62fcb8069005cda
Alexei Danchenkov <alexei.d@smpstaff.org>
2019-09-23 12:23:54 -0700

    updated with more examples from the book

--- webpack.config.js
+++ webpack.config.js
@@ -21,68 +21,68 @@ module.exports = {
     filename: '[name].js'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      'autoprefixer': {},
-      'moment': 'moment'
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-    })
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    'autoprefixer': {},
+    'moment': 'moment'
+  }),
+  new MiniCssExtractPlugin({
+    filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+    chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+  })
   ],
   module: {
     rules: [
+    {
+      test: /\.module\.s(a|c)ss$/,
+      loader: [
+      isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
       {
-        test: /\.module\.s(a|c)ss$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-              camelCase: true,
-              sourceMap: isDevelopment
-            }
-          }, {
-            loader: 'postcss-loader', // Run postcss actions
-            options: {
-              plugins: function () { // postcss plugins, can be exported to postcss.config.js
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-        ]
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+          camelCase: true,
+          sourceMap: isDevelopment
+        }
       }, {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
+        loader: 'postcss-loader', // Run postcss actions
+        options: {
+          plugins: function () { // postcss plugins, can be exported to postcss.config.js
+            return [
+            require('autoprefixer')
+            ];
           }
-        ]
+        }
       }, {
-        test: /\.css$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
+        loader: 'sass-loader',
+        options: {
+          sourceMap: isDevelopment
+        }
       }
+      ]
+    }, {
+      test: /\.s(a|c)ss$/,
+      exclude: /\.module.(s(a|c)ss)$/,
+      loader: [
+      isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+      'css-loader',
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: isDevelopment
+        }
+      }
+      ]
+    }, {
+      test: /\.css$/,
+      loader: [
+      isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+      'css-loader'
+      ]
+    }
     ]
   },
   resolve: {
