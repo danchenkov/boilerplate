ec1e946ad3b69d87595f11c9ae4261fc5c814e17
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-10-19 05:54:11 -0700

    updated with pages fiddle

--- assets/vue.js
+++ assets/vue.js
@@ -1,1 +1,112 @@
-!function(t){var e={};function r(n){if(e[n])return e[n].exports;var o=e[n]={i:n,l:!1,exports:{}};return t[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=t,r.c=e,r.d=function(t,e,n){r.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:n})},r.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)r.d(n,o,function(e){return t[e]}.bind(null,o));return n},r.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return r.d(e,"a",e),e},r.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},r.p="",r(r.s=6)}({0:function(t,e,r){t.exports=r.p+"main.css"},6:function(t,e,r){"use strict";r.r(e);r(0);$(document).ready((function(){Vue.config.devtools=!0,Vue.component("post",{props:["post"],template:"<h3>{{ post.title }}</h3>"}),new Vue({el:"#app",data:{posts:[{id:1,title:"Target A1"},{id:2,title:"Target B2"},{id:3,title:"Target C3"},{id:4,title:"Target D4"},{id:5,title:"Target E5"},{id:6,title:"Target F6"},{id:7,title:"Target G7"},{id:8,title:"Target H8"}]}})}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/vue.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/scss/main.scss":
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"main.css\";\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
+
+/***/ }),
+
+/***/ "./src/vue.js":
+/*!********************!*\
+  !*** ./src/vue.js ***!
+  \********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_main__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/main */ \"./src/scss/main.scss\");\n/* harmony import */ var _scss_main__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main__WEBPACK_IMPORTED_MODULE_0__);\n\n\n$(document).ready(function() {\n\tVue.config.devtools = true;\n\n\tVue.component(\"post\", {\n\t\tprops: [\"post\"],\n\t\ttemplate: \"<h3>{{ post.title }}</h3>\"\n\t});\n\n\tnew Vue({\n\t\tel: \"#app\",\n\t\tdata: {\n\t\t\tposts: [\n\t\t\t\t{ id: 1, title: \"Target A1\" },\n\t\t\t\t{ id: 2, title: \"Target B2\" },\n\t\t\t\t{ id: 3, title: \"Target C3\" },\n\t\t\t\t{ id: 4, title: \"Target D4\" },\n\t\t\t\t{ id: 5, title: \"Target E5\" },\n\t\t\t\t{ id: 6, title: \"Target F6\" },\n\t\t\t\t{ id: 7, title: \"Target G7\" },\n\t\t\t\t{ id: 8, title: \"Target H8\" }\n\t\t\t]\n\t\t}\n\t});\n\n\tnew Vue({\n\t\tel: \"#editor\",\n\t\tdata: {\n\t\t\tinput: \"# hello\"\n\t\t},\n\t\tcomputed: {\n\t\t\tcompiledMarkdown: function() {\n\t\t\t\treturn marked(this.input, { sanitize: true });\n\t\t\t}\n\t\t},\n\t\tmethods: {\n\t\t\tupdate: _.debounce(function(e) {\n\t\t\t\tthis.input = e.target.value;\n\t\t\t}, 300)\n\t\t}\n\t});\n});\n\n//# sourceURL=webpack:///./src/vue.js?");
+
+/***/ })
+
+/******/ });
