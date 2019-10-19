2c3687848eb37df49ac5ff462345a9785af7c4f1
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-10-18 20:22:31 -0700

    modular css

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,108 @@
-const path = require('path');
-const webpack = require('webpack');
-const isDevelopment = process.env.NODE_ENV !== 'production';
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const isDevelopment = process.env.NODE_ENV !== "production";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 module.exports = {
-	mode: isDevelopment ? 'development' : 'production',
-	performance: {
-	  hints: false
+	mode: isDevelopment ? "development" : "production",
+	devServer: {
+		port: 5005,
+		contentBase: "./",
+		watchOptions: {
+			poll: true
+		}
 	},
 	entry: {
-		main: './src/main.js',
-		vue: './src/vue.js'
+		main: "./src/main.js",
+		vue: "./src/vue.js"
 	},
 	output: {
-		path: path.resolve(__dirname, 'assets'),
-		filename: '[name].js'
+		path: path.resolve(__dirname, "assets"),
+		filename: "[name].js"
 	},
 	plugins: [
-	new webpack.ProvidePlugin({
-		'autoprefixer': {},
-		'moment': 'moment'
-	}),
-	// To strip all locales except “en”
-    // new MomentLocalesPlugin(),
-    // Or: To strip all locales except “en” and “ru”
-    // (“en” is built into Moment and can’t be removed)
-    new MomentLocalesPlugin({
-        localesToKeep: ['ru'],
-    }),
-
-	// new MiniCssExtractPlugin({
-	// 	filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-	// 	chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-	// })
+		new webpack.ProvidePlugin({
+			autoprefixer: {},
+			moment: "moment"
+		}),
+		// To strip all locales except “en”
+		// new MomentLocalesPlugin(),
+		// Or: To strip all locales except “en” and “ru”
+		// (“en” is built into Moment and can’t be removed)
+		new MomentLocalesPlugin({
+			localesToKeep: ["ru"]
+		}),
+		new MiniCssExtractPlugin({
+			filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+			chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css"
+		})
 	],
 	module: {
 		rules: [
 			{
-				test: /\.s(a|c)ss$/,
-				exclude: /\.module.(s(a|c)ss)$/,
-				use: [
+				test: /\.module\.s(a|c)ss$/,
+				loader: [
+					isDevelopment
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
 					{
-						loader: 'file-loader',
+						loader: "css-loader",
 						options: {
-							name: '[name].css',
+							modules: true,
+							localIdentName: "[name]__[local]___[hash:base64:5]",
+							camelCase: true,
+							sourceMap: isDevelopment
 						}
 					},
 					{
-						loader: 'extract-loader'
+						loader: "postcss-loader", // Run postcss actions
+						options: {
+							plugins: function() {
+								// postcss plugins, can be exported to postcss.config.js
+								return [require("autoprefixer")];
+							}
+						}
 					},
 					{
-						loader: 'css-loader?-url'
-					},
-					{
-						loader: 'postcss-loader'
-					},
-					{
-						loader: 'sass-loader'
+						loader: "sass-loader",
+						options: {
+							sourceMap: isDevelopment
+						}
 					}
+				]
+			},
+			{
+				test: /\.s(a|c)ss$/,
+				exclude: /\.module.(s(a|c)ss)$/,
+				loader: [
+					isDevelopment
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: isDevelopment
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				loader: [
+					isDevelopment
+						? "style-loader"
+						: MiniCssExtractPlugin.loader,
+					"css-loader"
 				]
 			}
 		]
 	},
 	resolve: {
-		extensions: ['.js', '.jsx', '.scss', '.css']
-		// extensions: ['.js', '.jsx']
+		extensions: [".js", ".jsx", ".scss", ".css"]
 	},
 	externals: {
-		$: 'jquery',
-		jQuery: 'jquery',
-		'window.jQuery': 'jquery',
-		Modernizr: 'modernizr'
+		Modernizr: "modernizr"
 	}
 };
