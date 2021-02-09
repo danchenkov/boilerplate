3b2a20f8c7c633a5286321de844ab6a739ca8a84
Alexei Danchenkov <alexeid@scientology.net>
2021-02-09 11:50:03 -0800

    refactoring: remove remote libraries and fonts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './scss/main';
+const bcrypt = require('./bcrypt')
 
 function setCheckedValueOfRadioButtonGroup(name, value) {
 	var radios = document.getElementsByName(name);
@@ -26,9 +26,26 @@ function setValueOfFormField(name, value) {
 	}
 }
 
-function setFlashMessage(id, value) {
+function setValueOfFormTextArea(name, value) {
+	var fields = document.getElementsByName(name);
+	for (var j = 0; j < fields.length; j++) {
+		fields[j].value = value;
+	}
+}
+
+function setFlashMessage(id, value, color) {
 	var flashMessage = document.getElementById(id);
+	if (value == null) {
+		flashMessage.style.display = "none";
+	}
+	if (color == "error") {
+		flashMessage.classList.add(color);
+	}
 	flashMessage.innerText = value;
+}
+
+function validTermsAttest() {
+	return document.getElementById("terms").checked;
 }
 
 $(document).ready(function() {
@@ -54,58 +71,75 @@ $(document).ready(function() {
 		setCheckedValueOfRadioButtonGroup('title', urlParams.get('title'))
 		setValueOfFormField('first_name', urlParams.get('first_name'))
 		setValueOfFormField('last_name', urlParams.get('last_name'))
+		setValueOfFormField('birthdate', urlParams.get('birthdate'))
 		setValueOfFormField('email', urlParams.get('email'))
 		setValueOfFormField('phone', urlParams.get('phone'))
 		setValueOfFormField('country', urlParams.get('country'))
-		setValueOfFormField('password', urlParams.get('password'))
+		setValueOfFormTextArea('bio', urlParams.get('bio'))
 		setCheckedValueOfCheckbox('terms', urlParams.get('terms'))
 		setFlashMessage('flash-message', urlParams.get('flash'))
+
+		// document.querySelector("#sample-form").addEventListener("submit", function(e){
+		document.getElementById("sample-form").addEventListener("submit", function(e){
+			if(!validTermsAttest()){
+				// setFlashMessage('flash-message', "Please attest to the Terms of Use", "error")
+				// document.getElementById("form").scrollIntoView();
+				alert("Please indicate that you accept the Terms and Conditions");
+		        e.preventDefault();
+		        return false;
+		    }
+		    setValueOfFormField("hash", bcrypt.hash(document.getElementById("password")));
+		});
+
+		document.getElementById("reset").addEventListener("click", function(e){
+			window.history.replaceState(null, null, window.location.pathname + window.location.hash);
+		});
+
+
+		$(function() {
+			$('textarea[max-rows]').each(function () {
+				var textarea = $(this);
+
+				var minRows = Number(textarea.attr('rows'));
+				var maxRows = Number(textarea.attr('max-rows'));
+
+				// clone the textarea and hide it offscreen
+				// TODO: copy all the styles
+				var textareaClone = $('<textarea/>', {
+					rows: minRows,
+					maxRows: maxRows,
+					class: textarea.attr('class')
+				}).css({
+					position: 'absolute',
+					left: -$(document).width() * 2
+				}).insertAfter(textarea);
+
+				var textareaCloneNode = textareaClone.get(0);
+
+				textarea.on('input', function () {
+					// copy the input from the real textarea
+					textareaClone.val(textarea.val());
+
+					// set as small as possible to get the real scroll height
+					textareaClone.attr('rows', 1);
+
+					// save the real scroll height
+					var scrollHeight = textareaCloneNode.scrollHeight;
+
+					// increase the number of rows until the content fits
+					for (var rows = minRows; rows < maxRows; rows++) {
+						textareaClone.attr('rows', rows);
+
+						if (textareaClone.height() > scrollHeight) {
+							break;
+						}
+					}
+
+					// copy the rows value back to the real textarea
+					textarea.attr('rows', textareaClone.attr('rows'));
+				}).trigger('input');
+			});
+		})
+
 	}
-
-	$(function() {
-	$('textarea[max-rows]').each(function () {
-		var textarea = $(this);
-
-		var minRows = Number(textarea.attr('rows'));
-		var maxRows = Number(textarea.attr('max-rows'));
-
-		// clone the textarea and hide it offscreen
-		// TODO: copy all the styles
-		var textareaClone = $('<textarea/>', {
-			rows: minRows,
-			maxRows: maxRows,
-			class: textarea.attr('class')
-		}).css({
-			position: 'absolute',
-			left: -$(document).width() * 2
-		}).insertAfter(textarea);
-
-		var textareaCloneNode = textareaClone.get(0);
-
-		textarea.on('input', function () {
-			// copy the input from the real textarea
-			textareaClone.val(textarea.val());
-
-			// set as small as possible to get the real scroll height
-			textareaClone.attr('rows', 1);
-
-			// save the real scroll height
-			var scrollHeight = textareaCloneNode.scrollHeight;
-
-			// increase the number of rows until the content fits
-			for (var rows = minRows; rows < maxRows; rows++) {
-				textareaClone.attr('rows', rows);
-
-				if (textareaClone.height() > scrollHeight) {
-					break;
-				}
-			}
-
-			// copy the rows value back to the real textarea
-			textarea.attr('rows', textareaClone.attr('rows'));
-		}).trigger('input');
-	});
 });
-});
-
-
