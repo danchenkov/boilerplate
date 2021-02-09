b8d07178d6c50362d060a4f9a0d67a631ebcd773
Alexei Danchenkov <alexeid@scientology.net>
2021-02-08 17:35:36 -0800

    style updates

--- src/main.js
+++ src/main.js
@@ -26,6 +26,11 @@ function setValueOfFormField(name, value) {
 	}
 }
 
+function setFlashMessage(id, value) {
+	var flashMessage = document.getElementById(id);
+	flashMessage.innerText = value;
+}
+
 $(document).ready(function() {
 	if (Modernizr) {
 		console.log("Modernizr is active");
@@ -54,9 +59,53 @@ $(document).ready(function() {
 		setValueOfFormField('country', urlParams.get('country'))
 		setValueOfFormField('password', urlParams.get('password'))
 		setCheckedValueOfCheckbox('terms', urlParams.get('terms'))
+		setFlashMessage('flash-message', urlParams.get('flash'))
 	}
 
+	$(function() {
+	$('textarea[max-rows]').each(function () {
+		var textarea = $(this);
 
+		var minRows = Number(textarea.attr('rows'));
+		var maxRows = Number(textarea.attr('max-rows'));
+
+		// clone the textarea and hide it offscreen
+		// TODO: copy all the styles
+		var textareaClone = $('<textarea/>', {
+			rows: minRows,
+			maxRows: maxRows,
+			class: textarea.attr('class')
+		}).css({
+			position: 'absolute',
+			left: -$(document).width() * 2
+		}).insertAfter(textarea);
+
+		var textareaCloneNode = textareaClone.get(0);
+
+		textarea.on('input', function () {
+			// copy the input from the real textarea
+			textareaClone.val(textarea.val());
+
+			// set as small as possible to get the real scroll height
+			textareaClone.attr('rows', 1);
+
+			// save the real scroll height
+			var scrollHeight = textareaCloneNode.scrollHeight;
+
+			// increase the number of rows until the content fits
+			for (var rows = minRows; rows < maxRows; rows++) {
+				textareaClone.attr('rows', rows);
+
+				if (textareaClone.height() > scrollHeight) {
+					break;
+				}
+			}
+
+			// copy the rows value back to the real textarea
+			textarea.attr('rows', textareaClone.attr('rows'));
+		}).trigger('input');
+	});
+});
 });
 
 
